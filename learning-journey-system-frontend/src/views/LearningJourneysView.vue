5641c33a70af8847638ad3bf82473d3e96263ab3
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-09-28 01:20:54 +0800

    added navbar and sample routes

---
+++ learning-journey-system-frontend/src/views/LearningJourneysView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+        <Navbar />
+        <h1>This is Learning Journeys Page</h1>
+    </div>
+</template>
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+    name: 'LearningJourneysView',
+    components: {
+        Navbar
+    }
+}
+</script>
