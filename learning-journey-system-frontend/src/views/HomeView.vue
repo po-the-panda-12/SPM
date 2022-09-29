6b0e114902c363cce5aa67a0a2c150d83b764ef0
Ming Rong <kwekmingrong@yahoo.com.sg>
2022-09-30 00:37:56 +0800

    Forget to add navbar to homepage.vue

--- learning-journey-system-frontend/src/views/HomeView.vue
+++ learning-journey-system-frontend/src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
+    <Navbar />
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <h1>This is the home page</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Navbar.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navbar
   }
 }
 </script>
