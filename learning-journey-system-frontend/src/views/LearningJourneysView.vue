098971e7dcb402377690d2d2940026eb9f5933ab
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-09-29 19:36:21 +0800

    WIP

--- learning-journey-system-frontend/src/views/LearningJourneysView.vue
+++ learning-journey-system-frontend/src/views/LearningJourneysView.vue
@@ -2,6 +2,7 @@
     <div class="about">
         <Navbar />
         <h1>This is Learning Journeys Page</h1>
+        
         <LearningJourneyCard v-for="learningJourney in userLearningJourneys" v-bind:learningJourney="learningJourney"/>
         <!-- <div>
             {{ userLearningJourneys }}
@@ -11,6 +12,7 @@
 <script>
 import Navbar from '@/components/Navbar.vue'
 import LearningJourneyCard from '@/components/LearningJourneyCard.vue'
+
 export default {
     name: 'LearningJourneysView',
     data() {
@@ -21,6 +23,7 @@ export default {
     mounted() {
         axios.get("https://jdvmt1fgol.execute-api.us-west-1.amazonaws.com/api/journey")
         .then(response => this.userLearningJourneys = response.data.data.learning_journey)
+        .then(response => console.log(response))
     },
     components: {
         Navbar,
