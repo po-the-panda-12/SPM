cf338ffc74b2410a391855074fa2ea9a4785043d
Ming Rong <kwekmingrong@yahoo.com.sg>
2022-09-30 00:26:13 +0800

    update

--- learning-journey-system-frontend/src/router/index.js
+++ learning-journey-system-frontend/src/router/index.js
@@ -14,12 +14,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-  {
-    path: '/viewCoursesBySkills',
-    name: 'viewCoursesBySkills',
-    component: viewCoursesBySkills
-  }
+  },
 ]
 
 const router = createRouter({
