545655289fa28ba3ecfd1e6e8b49d1a511778e41
Ming Rong <kwekmingrong@yahoo.com.sg>
2022-09-29 20:30:32 +0800

    added skill tab to navbar & link path at index.js

--- learning-journey-system-frontend/src/router/index.js
+++ learning-journey-system-frontend/src/router/index.js
@@ -15,6 +15,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
+  {
+    path: '/viewCoursesBySkills',
+    name: 'viewCoursesBySkills',
+    component: viewCoursesBySkills
+  }
 ]
 
 const router = createRouter({
