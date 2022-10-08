727e978d368eb08a27e8ed1fe7b87c60cbc0f869
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-10-08 11:52:43 +0800

    removed comments for Navbar.vue

--- learning-journey-system-frontend/src/components/Navbar.vue
+++ learning-journey-system-frontend/src/components/Navbar.vue
@@ -14,31 +14,24 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item px-4">
-                        <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
                         <router-link to="/">Home</router-link>
                     </li>
                     <li class="nav-item px-4">
-                        <!-- <a class="nav-link" href="#">Link</a> -->
                         <router-link to="/about">About</router-link>
                     </li>
                     <li class="nav-item px-4">
-                        <!-- <a class="nav-link" href="#">Link</a> -->
                         <router-link to="/learningJourneys"> My Learning Journeys</router-link>
                     </li>
                     <li class="nav-item px-4">
-                        <!-- <a class="nav-link" href="#">Link</a> -->
                         <router-link to="/jobroles">Job Roles</router-link>
                     </li>
                     <li class="nav-item px-4">
-                        <!-- <a class="nav-link" href="#">Link</a> -->
                         <router-link to="/">Skills</router-link>
                     </li>
                     <li class="nav-item px-4">
-                        <!-- <a class="nav-link" href="#">Link</a> -->
                         <router-link to="/">Courses</router-link>
                     </li>
                     <li class="nav-item px-4">
-                        <!-- <a class="nav-link" href="#">Link</a> -->
                         <router-link to="/viewSkillsandCourses">View Skills and Courses</router-link>
                     </li>
                 </ul>
