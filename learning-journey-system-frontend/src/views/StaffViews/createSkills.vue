bb1d492bc44f96806e280ffc11097f0857a944a9
Ming Rong <kwekmingrong@yahoo.com.sg>
2022-10-12 21:41:21 +0800

    added axios to createSkills.vue

--- learning-journey-system-frontend/src/views/StaffViews/createSkills.vue
+++ learning-journey-system-frontend/src/views/StaffViews/createSkills.vue
@@ -3,7 +3,8 @@
         <form>
             <div class="form-group mt-4">
                 <h1 class = "mb-4"> Add New Skill </h1>
-                <input type="skill"  class="input-lg form-control " id="skill" placeholder="Enter skill" style="width: 100%; height: 50px">
+                <input v-model="skill_name" type ='text' class="input-lg form-control " id="skill" placeholder="Enter skill" style="width: 100%; height: 50px">
+                <p> {{ skill_name }} </p>
             </div>
             <div> 
                 <h5 class = "mt-4">Status</h5>
@@ -11,7 +12,7 @@
             </div>
             
             <div class="d-grid gap-2 mt-4 col-6 mx-auto">
-                <button class="btn btn-primary" type="button">Save</button>
+                <button @click='createSkill()' class="btn btn-primary" type="button">Save</button>
             </div>
         </form>
     </div>
@@ -26,6 +27,27 @@
 
     export default {
         name: 'createSkills',
-
+    
+    data(){
+        return {
+            skill_name: ''
+        }
+    },
+    methods: {
+            createSkill() {
+            //POST skill to API endpoint https://jdvmt1fgol.execute-api.us-west-1.amazonaws.com/api/skill
+                axios.post('https://jdvmt1fgol.execute-api.us-west-1.amazonaws.com/api/skill'
+                ,{
+                    name: this.skill_name
+                })
+                .then (response => {
+                    console.log(response.data)
+                })
+                .catch (error => {
+                    console.log(error)
+                })
+            }
+        }
     }
+
 </script>
