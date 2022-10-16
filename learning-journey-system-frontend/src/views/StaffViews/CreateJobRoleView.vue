3844baa850196972976f5933de21323137ef4ee1
teoruixin <ruixin.teo.2020@scis.smu.edu.sg>
2022-10-16 16:24:09 +0800

    close to finishing IG26, error with slow aws db

--- learning-journey-system-frontend/src/views/StaffViews/CreateJobRoleView.vue
+++ learning-journey-system-frontend/src/views/StaffViews/CreateJobRoleView.vue
@@ -7,19 +7,21 @@
         <div>
             <h3 class="fs-5 mt-4 mb-3"><span style="color:red;">*</span> Role Name</h3>
             
-            <!-- check if role name already exists -->
-            <div v-if="jobroleNames.includes(role_name)">
-                <p>{{role_name}}</p>
-            </div>
-            <div class="input-group mb-4 input-group-lg">
+            <div class="input-group mb-3 input-group-lg">
                 <input type="text" class="form-control" placeholder="Your role name goes here....." aria-label="Username" v-model="role_name">
+            </div>
+            <!-- check if role name already exists -->
+            <div v-for="name in jobroleNames">
+                <!-- <p>{{name.role_name}}</p> -->
+                <p class="mb-3" v-if="name.role_name.toLowerCase() === role_name.toLowerCase()" style="color:red">This job role already exists. Enter another role name</p>
             </div>
 
             <h3 class="fs-5 my-3"><span style="color:red;">*</span> Skills</h3>
             <div class="mb-3">
                 <div v-for="selected_skill in role_skills" class="d-inline">
-                    <span class="badge bg-primary mx-1 mt-2 mb-3">{{selected_skill.skill_name}}</span>
+                    <span class="badge bg-primary mx-1 mb-3">{{selected_skill.skill_name}}</span>
                 </div>
+                <!-- Search bar and list of skills -->
                 <input type="text" class="form-control mb-3" v-model="skill" placeholder="Search skills"/>
                 <div class="form-check" v-for="skill in filteredSkills">
                     <input class="form-check-input" type="checkbox" :value="skill" :id="skill.skill_id" v-model="role_skills">
@@ -29,7 +31,9 @@
                 </div>
             </div>
 
-            <button class="btn btn-primary mt-4 px-5" @click="addJobRole(); addSkilltoJobRole()">Save</button>
+            <div class="text-center mb-5">
+                <button class="btn btn-primary mt-4" style="padding: 10px 15%" @click="addJobRole(); activate()">Save</button>
+            </div>
 
         </div>
     </div>
@@ -45,7 +49,7 @@
       data() {
         return {
             role_name: '',
-            existing_roleName: [], // existing job role names
+            existing_roles: [], // existing job role names
             skill: '', // search bar
             role_skills: [], // skills selected by the user
             all_skills: [], // all skills in the database
@@ -54,37 +58,56 @@
         },
         methods: {
             addJobRole(){
-                axios.post('https://jdvmt1fgol.execute-api.us-west-1.amazonaws.com/api/role', {
-                    role_name: this.role_name,
-                })
-                .then(response => {
-                    console.log(response)
-                
-                })
-                .catch(error => alert(error));
+                // if role_name exists in jobroleNames, then don't add it
+                if(this.jobroleNames.some(name => name.role_name.toLowerCase() === this.role_name.toLowerCase())){
+                    alert("This job role already exists")
+                }
+                else if(this.role_name == ''){
+                    alert("Please enter a role name")
+                }
+                else{
+                    axios.post('https://jdvmt1fgol.execute-api.us-west-1.amazonaws.com/api/role', {
+                        name: this.role_name
+                    })
+                    .then(response => {
+                        console.log(response)
+                    
+                    })
+                    .catch(error => alert(error));
 
+                }
+            },
+
+            getNewJobRoleID(){
                 axios.get('https://jdvmt1fgol.execute-api.us-west-1.amazonaws.com/api/role')
-                .then(response => {
-                    console.log(response.data.data.job_roles.slice(-1)[0].role_id)
-                    this.role_id = response.data.data.job_roles.slice(-1)[0].role_id
-                })
-                .catch(error => alert(error));
+                    .then(response => {
+                        console.log(response.data.data.job_roles.slice(-1)[0].role_id)
+                        this.role_id = response.data.data.job_roles.slice(-1)[0].role_id
+                    })
+                    .catch(error => alert(error));
             },
 
             getJobRole(){
                 axios.get('https://jdvmt1fgol.execute-api.us-west-1.amazonaws.com/api/role')
                 .then(response => {
                     console.log(response.data.data.job_roles)
-                    this.existing_roleName = response.data.data.job_roles
+                    this.existing_roles = response.data.data.job_roles
                 })
                 .catch(error => alert(error));
             },
 
-            addSkilltoJobRole(){
+            activate(){
+                setTimeout(this.getNewJobRoleID, 300000)
+                // set timer to call addskilltojobrole
+                setTimeout(this.addSkilltoJobRole, 300000)
+            },
+
+            addSkilltoJobRole: async function(){
                 for (var i = 0; i < this.role_skills.length; i++){
-                    axios.post('https://jdvmt1fgol.execute-api.us-west-1.amazonaws.com/api/role/skill', {
-                        role_id: this.role_id,
-                        skill_id: this.role_skills[i].skill_id
+                    console.log(this.role_skills[i].skill_id)
+                    await axios.post('https://jdvmt1fgol.execute-api.us-west-1.amazonaws.com/api/role_skill', {
+                        role: this.role_id,
+                        skill: this.role_skills[i].skill_id
                     })
                     .then(response => {
                         console.log(response)
@@ -118,7 +141,7 @@
             },
             // for checking if the job role name already exists
             jobroleNames() {
-                return this.existing_roleName.filter(p => {
+                return this.existing_roles.filter(p => {
                     return p.role_name.toLowerCase().indexOf(this.role_name.toLowerCase()) != -1;
                 });
             }
