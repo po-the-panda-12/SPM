ebfcc40d652856145eb28dfeef7f09bc8309f6a2
Ming Rong <kwekmingrong@yahoo.com.sg>
2022-10-16 16:14:37 +0800

    shifted viewAllSkills.vue to AdminViews folder

--- learning-journey-system-frontend/src/router/index.js
+++ learning-journey-system-frontend/src/router/index.js
@@ -5,7 +5,7 @@ import JobRoleView from '../views/StaffViews/JobRoleView.vue'
 import IndivLearningJourneyView from '../views/StaffViews/IndivLearningJourneyView.vue'
 import CreatelearningJourneyView from '../views/StaffViews/CreatelearningJourneyView.vue'
 import viewSkillsandCourses from '../views/StaffViews/viewSkillsandCourses.vue'
-import viewAllSkills from '../views/StaffViews/viewAllSkills.vue'
+import viewAllSkills from '../views/AdminViews/viewAllSkills.vue'
 
 
 const routes = [
