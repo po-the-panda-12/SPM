1740d4b2cb30e905153de6f1ed7302c8d957bc32
Jerdwut <Jerdwutr.2020@scis.smu.edu.sg>
2022-10-17 17:43:58 +0800

    Style update

--- learning-journey-system-frontend/src/views/StaffViews/StaffViewCourses.vue
+++ learning-journey-system-frontend/src/views/StaffViews/StaffViewCourses.vue
@@ -3,14 +3,16 @@
         <h1 class = 'text-left mt-4'> View all Courses </h1> 
         <div class = 'row'>
             <div v-for = 'course in courses' :key="course.course_id" class= 'mt-4 mb-4 col-4'>
-                <div class="card">
+                <div class="card" style="height: 450px;">
                     <div class="card-body">
-                    <img class='rounded mx-auto d-block' src="@/assets/courses.png">
+                        <img class='rounded mx-auto d-block' src="@/assets/courses.png">
+                    
+                        <div class="ps-2 mt-2 text-center">Course Name: {{course.course_name}}</div>
+                        <div class="ps-2 mt-2 text-center">Description: {{course.course_desc}}</div>
+                        <div class="ps-2 mt-2 text-center"> Type: {{course.course_type}}</div>
+                        <div class="ps-2 mt-2 text-center"> Category: {{course.course_category}}</div>
                     </div>
-                    <text class="ps-2 mt-2 text-center">Course Name: {{course.course_name}} </text>
-                    <text class="ps-2 mt-2 text-center">Description: {{course.course_desc}} </text>
-                    <text class="ps-2 mt-2 text-center"> Type: {{course.course_type}} </text>
-                    <text class="ps-2 mt-2 text-center"> Category: {{course.course_category}} </text>
+                    
                 </div>
             </div>
         </div>    
