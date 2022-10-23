784f6184563ce2f0a2e357f2cbf2af10cd83f8dd
genevievetai2020 <89062430+genevievetai2020@users.noreply.github.com>
2022-10-24 02:41:30 +0800

    minor update to navbar

--- learning-journey-system-frontend/src/components/Common/Navbar.vue
+++ learning-journey-system-frontend/src/components/Common/Navbar.vue
@@ -31,9 +31,6 @@
                     <li class="nav-item px-4">
                         <router-link to="/StaffViewCourses">Courses</router-link>
                     </li>
-                    <li v-if="currentStaffRole == 'Admin'" class="nav-item px-4">
-                        <router-link to="/courses">Courses</router-link>
-                    </li>
                     <!-- <li class="nav-item px-4">
                         <router-link to="/viewSkillsandCourses">View Skills and Courses</router-link>
                     </li> -->
