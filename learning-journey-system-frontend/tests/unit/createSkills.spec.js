71e68639098a4b8eb4a950d7552b11b6a1842b4d
Ming Rong <mrkwek.2020@scis.smu.edu.sg>
2022-10-31 10:51:37 +0800

    update response code

--- learning-journey-system-frontend/tests/unit/createSkills.spec.js
+++ learning-journey-system-frontend/tests/unit/createSkills.spec.js
@@ -7,7 +7,7 @@ const createNewSkill = async() => {
     }
     let response = await axios.post('https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/skill', newSkill)
     console.log(response)
-    return response.status
+    return response.data.code
 }
 
 const createExistingSkill = async() => {
@@ -16,7 +16,7 @@ const createExistingSkill = async() => {
     }
     let response = await axios.post('https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/skill', newSkill)
     console.log(response)
-    return response.status
+    return response.data.code
 }
 
 describe("createSkills.vue", () => {
