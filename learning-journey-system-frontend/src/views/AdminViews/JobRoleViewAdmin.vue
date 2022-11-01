15e6c609b965f835d2348eeb3f150175a1d4800d
teoruixin <ruixin.teo.2020@scis.smu.edu.sg>
2022-11-01 11:51:15 +0800

    minor edit

--- learning-journey-system-frontend/src/views/AdminViews/JobRoleViewAdmin.vue
+++ learning-journey-system-frontend/src/views/AdminViews/JobRoleViewAdmin.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="jobroles container-fluid position-relative align-self-stretch p-5">
-            <div class="d-flex mb-3 mt-2">
+            <div class="d-flex mb-3">
                 <div class="fs-2 fw-bold me-auto">Job Roles Available</div> 
                 <div class="fs-6 ms-auto"><router-link :to="'/createjobrole'" class="btn btn-outline-dark m-1"><i class="far fa-plus"></i>&nbsp; Add Job Role</router-link></div>
             </div>
