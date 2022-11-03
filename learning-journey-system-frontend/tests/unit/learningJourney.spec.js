33844639128986e18a719b84a59b99a4a7aa2ead
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-11-03 14:19:23 +0800

    learningJourney.spec.js completed for CRD functions

--- learning-journey-system-frontend/tests/unit/learningJourney.spec.js
+++ learning-journey-system-frontend/tests/unit/learningJourney.spec.js
@@ -8,7 +8,6 @@ const getAllLearningJourneys = async () => {
 
 const getIndividualLearningJourney = async () => {
     const response = await axios.get("https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/journey?id=2")
-    console.log("here", response.data.code)
     return response.data.code
 }
 
@@ -20,6 +19,22 @@ const createNewLearningJourney = async (mockData) => {
 const deleteLearningJourney = async (lj_id) => {
     const response = await axios.delete("https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/journey", {data: {id: lj_id}})
     return response.data.code
+}
+
+const getLatestLJID = async () => {
+    const response = await axios.get("https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/journey")
+    
+    const allLearningJourneys = response.data.data.learning_journey;
+    
+    // iterate through all learning journeys and find the latest one
+    let latestLJID = 0
+    for (let i = 0; i < allLearningJourneys.length; i++) {
+        if (allLearningJourneys[i].lj_id > latestLJID) {
+            latestLJID = allLearningJourneys[i].lj_id
+        }
+    }
+    console.log(latestLJID)
+    return latestLJID
 }
 
 describe("Learning Journeys", () => {
@@ -34,19 +49,42 @@ describe("Learning Journeys", () => {
         expect(response).toBe(200)
     })
 
-    // describe("Creating and updating Learning Journeys", () => {
-    //     it("should return response of 200 when creating individual learning journey", async () => {
-    //         const mockData = {
-    //             "name": "Mock Learning Journey",
-    //             "staff_id": 3,
-    //             "role": 3
-    //         }
-    //         const response = await createNewLearningJourney(mockData)
-    //         expect(response).toBe(200)
-    //     })
-    //     afterAll(asycn () => {
-    //         deleteLearningJourney
-
-    //     })
+    // it("should return the latest LJID", async () => {
+    //     const response = await getLatestLJID()
+    //     console.log("here3", response)
+    //     expect(response).toBe(3)
     // })
+
+    describe("Creating Learning Journeys", () => {
+        it("should return response of 200 when creating individual learning journey", async () => {
+            const mockData = {
+                "name": "Mock Learning Journey",
+                "staff_id": 130002,
+                "role": 3
+            }
+            const response = await createNewLearningJourney(mockData)
+            expect(response).toBe(200)
+        })
+        afterAll(async () => {
+            const latestLJID = await getLatestLJID()
+            await deleteLearningJourney(latestLJID)
+        })
+    })
+
+    describe("Deleting Learning Journeys", () => {
+        beforeAll(async () => {
+            const mockData = {
+                "name": "Mock Learning Journey",
+                "staff_id": 130002,
+                "role": 3
+            }
+            await createNewLearningJourney(mockData)
+        })
+
+        it("Should return 200 after deleting the latest created learning journey", async () => {
+            const latestLJID = await getLatestLJID()
+            const response = await deleteLearningJourney(latestLJID)
+            expect(response).toBe(200)
+        })
+    })
 })
