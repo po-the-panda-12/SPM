4c6045d43ae5a432c4922ec6a99547c375e0bf16
genevievetai2020 <89062430+genevievetai2020@users.noreply.github.com>
2022-11-04 22:26:24 +0800

    add await

--- learning-journey-system-frontend/src/views/StaffViews/viewSkillsandCourses.vue
+++ learning-journey-system-frontend/src/views/StaffViews/viewSkillsandCourses.vue
@@ -160,53 +160,36 @@
       },
 
       async saveCourses(){
-        if(this.selectedCourses.length > 0){
-          const name = this.$store.state.currentLJName
-          const staffId = this.$store.state.stored_staff_id
-          
-          if(name != null && this.role_id != null){
-          const data = {
-              "name": name,
-              "staff_id": staffId,
-              "role": this.role_id 
-          }
-          await axios.post("https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/journey", data)
-          .then(response => {
-              if(response.status === 200){
-                  alert('New Learning Journey Created Successfully')
-                  
-              }
-          })
-          .catch(error => {
-              console.log(error)
-          })
-          await this.getNewLJid()
-        }
-
-        for (var i = 0; i < this.selectedCourses.length; i++){
-          const course = this.selectedCourses[i]
-          const data = {
-            "lj": this.lj_id,
-            "course": course.course_id
-          }
-          console.log(data)
-          axios.post("https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/journey_course", data)
-          .then(response => {
-            if(response.status === 200){
-              alert("Course added to learning journey successfully!")
-            }
-          })
-          .catch(error => alert(error));
-        }
-
+        await this.createLJ()
+        await this.addCourseToLJ()  
         this.$store.commit('setRoleId', null)
         this.$store.commit('setIndivLJId', this.lj_id)
         this.$router.push({ path: '/indivlearningJourneys' })
+                
+      },
+
+      async createLJ(){
+        const name = this.$store.state.currentLJName
+        const staffId = this.$store.state.stored_staff_id
+          
+        if(name != null && this.role_id != null){
+        const data = {
+            "name": name,
+            "staff_id": staffId,
+            "role": this.role_id 
         }
-        else{
-          alert("Please select at least one course")
-        }
-        
+        await axios.post("https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/journey", data)
+        .then(response => {
+            if(response.status === 200){
+                alert('New Learning Journey Created Successfully')
+                
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        await this.getNewLJid()
+        } 
       },
       async getNewLJid(){
         await axios.get('https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/journey')
@@ -215,6 +198,24 @@
             console.log(this.lj_id)
         })
         .catch(error => alert(error));
+      },
+
+      async addCourseToLJ(){
+        for (var i = 0; i < this.selectedCourses.length; i++){
+          const course = this.selectedCourses[i]
+          const data = {
+            "lj": this.lj_id,
+            "course": course.course_id
+          }
+          console.log(data)
+          await axios.post("https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/journey_course", data)
+          .then(response => {
+            if(response.status === 200){
+              alert("Course added to learning journey successfully!")
+            }
+          })
+          .catch(error => alert(error));
+        }
       },
 
       async getExistingCoursesForLJ(lj_id){
@@ -226,22 +227,22 @@
         .catch(error => alert(error));
       }
     },
-    mounted() {
-      this.role_id = this.$store.state.stored_role_id
-      if(this.$store.state.current_lj != null){
-        this.lj_id = this.$store.state.stored_indivLJ_id
-        this.getExistingCoursesForLJ(this.lj_id)
-      }
-      this.getSkills(this.role_id);
-    },
     computed: {
       viewSelectedCourses() {
         return this.selectedCourses.length > 0 ? true : false
       }
     },
-    created(){
+    async created(){
         if(!this.$store.state.stored_current_accessrole){
         this.$router.push('/')
+        }
+        else {
+          this.role_id = this.$store.state.stored_role_id
+          if(this.$store.state.current_lj != null){
+            this.lj_id = this.$store.state.stored_indivLJ_id
+            await this.getExistingCoursesForLJ(this.lj_id)
+          }
+          await this.getSkills(this.role_id);
         }
     }    
 }
