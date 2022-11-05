23cbebfae2b6fe7962e2dd0c3d1914ee8d20d790
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-11-05 15:58:00 +0800

    added test cases for roleSKill.spec.js

--- learning-journey-system-frontend/tests/unit/roleSkill.spec.js
+++ learning-journey-system-frontend/tests/unit/roleSkill.spec.js
@@ -1,100 +1,93 @@
 import axios from 'axios'
-
-
 //CREATE
 
 //Assign skill id 1 to role id 3
-const newSkillAssign = async() => {
-    const newSkillAssign = {
-        "role" : 3,
-        "skill" : 1
+const assignSkillToRole = async(role_id, skill_id) => {
+    const payload = {
+        "role" : role_id,
+        "skill" : skill_id
     }
-    let response = await axios.post('https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/role_skill', newSkillAssign)
-    // console.log(response)
-    return response.data.code
-}
-
-//Assign skill id 1 to role id 3 to test for 500 response
-const ExistingSkillAssign = async() => {
-    const ExistingSkillAssign = {
-        "role" : 3,
-        "skill" : 1
-    }
-    let response = await axios.post('https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/role_skill', ExistingSkillAssign)
-    // console.log(response)
+    const response = await axios.post('https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/role_skill', payload)
+    // console.log("here", response)
     return response.data.code
 }
 
 
 //DELETE
 //To delete skill id 1 from role id 3
-const deleteExistingSkill = async() => {
-    const existingRoleSkill = {
-        "role" : 3,
-        "skill" : 1
+const removeSkillFromRole = async (role_id, skill_id) => {
+    const payload = {
+        "role" : role_id,
+        "skill" : skill_id
     }
-    let response = await axios.delete('https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/role_skill', existingRoleSkill)
-    // console.log(response)
-    return response.data.code
-}
-//To delete skill id 1 from role id 3 to test for 500 response
-const deleteNewSkill = async() => {
-    const newRoleSkill = {
-        "role": 3,
-        "skill": 1
-    }
-    let response = await axios.delete('https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/role_skill', newRoleSkill)
-    // console.log(response)
+    const response = await axios.delete('https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/role_skill', { data: payload })
+    // console.log("remove", response)
     return response.data.code
 }
 
 //READ
 //To retreive all role skills
 const getAllRoleSkills = async () => {
-  const result = []
-  let response = await axios.get('https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/role_skill')
-  //loop through first 3 response
-  for (let i = 0; i < 3; i++) {
-    result.push(response.data.data.role_skill[i])
-    }
-    return result
+    let response = await axios.get('https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/role_skill')
+    return response.data.code
+}
+//To retreive all role skills for role id 3
+const getRolesWitbSpecificSkillId = async (skill_id) => {
+    let response = await axios.get("https://3hcc44zf58.execute-api.ap-southeast-1.amazonaws.com/api/role_skill/role?role="+skill_id)
+    return response.data.code
 }
 
-// Assgin Skill to Role
+// Viewing of role skills - updates to role id 3 and skill id 1 will be reflected 
 describe("JobRoleView.vue", () => {
-    it("Should return 200 showing skill assigned to role successfully", async () => {
-        const response = await newSkillAssign()
-        expect(response).toEqual(200)
+    it('should return 200 showing all role skills', async () => {
+        const response = await getAllRoleSkills()
+        expect(response).toBe(200)
+    })
+    it('should return 200 showing all roles with specific skill id 1', async () => {
+        const response = await getRolesWitbSpecificSkillId(1)
+        expect(response).toBe(200)
     })
 
-    it('Should return 500 because skill already assigned to role', async () => {
-        const response = await ExistingSkillAssign()
-        expect(response).toEqual(500)
-    })
-})
-
-// // Viewing of role skills - updates to role id 3 and skill id 1 will be reflected 
-describe("JobRoleView.vue", () => {
-  it('Should render skill name and status', async () => {
-      const role_skillArray = await getAllRoleSkills()
-      const role_skill = [{"role_id": 1, "skill_id": 1}, {"role_id": 3, "skill_id": 1}, {"role_id": 3, "skill_id": 2}]
-      console.log("skills", role_skill)
-      
-      expect(role_skillArray).toEqual(role_skill)  
-  })
-})
-
-
-// Delete skill from role
-describe("JobRoleView.vue", () => {
-    it("Should return 200 showing skill was successfully deleted from role", async () => {
-        const response = await deleteExistingSkill()
-        expect(response).toEqual(200)
+    describe("add skill with skill_id 1 to role_id 3", () => {
+        it('Should return 200 showing skill assigned to role successfully', async () => {
+            const skill_id = 1
+            const role_id = 3
+            const response = await assignSkillToRole(role_id, skill_id)
+            expect(response).toBe(200)
+        })
+    
+        it('Should return 500 because skill_id 1 has already assigned to role_id 3', async () => {
+            const skill_id = 1
+            const role_id = 3
+            const response = await assignSkillToRole(role_id, skill_id)
+            expect(response).toBe(500)
+        })
+        // remove skill id 1 from role id 3
+        afterAll(async () => {
+            const skill_id = 1
+            const role_id = 3
+            await removeSkillFromRole(role_id, skill_id)
+        })
     })
 
-    it("Should return 404 showing skill was not in role", async () => {
-        const response = await deleteNewSkill()
-        expect(response).toEqual(404)
+    describe("remove skill with skill_id 1 from role_id 3", () => {
+        // assign skill id 1 to role id 3
+        beforeAll(async () => {
+            const skill_id = 1
+            const role_id = 3
+            await assignSkillToRole(role_id, skill_id)
+        })
+        it('Should return 200 showing skill_id 1 removed from role_id 3 successfully', async () => {
+            const skill_id = 1
+            const role_id = 3
+            const response = await removeSkillFromRole(role_id, skill_id)
+            expect(response).toBe(200)
+        })
+        it('Should return 500 because skill_id 1 has already removed from role_id 3', async () => {
+            const skill_id = 1
+            const role_id = 3
+            const response = await removeSkillFromRole(skill_id, role_id)
+            expect(response).toBe(404)
+        })
     })
-
 })
